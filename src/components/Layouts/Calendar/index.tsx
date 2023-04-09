5d65b64abb76b43dd0108f9bfb2ea8e0c526e06f
Vorobyshek228 <ivanobych@gmail.com>
2023-04-09 20:47:47 +0300

    add functional buttons

--- src/components/Layouts/Calendar/index.tsx
+++ src/components/Layouts/Calendar/index.tsx
@@ -1,42 +1,28 @@
 import dayGridPlugin from '@fullcalendar/daygrid' 
 import FullCalendar from "@fullcalendar/react"
 import timeGridPlugin from '@fullcalendar/timegrid'
-import { useRef, useState } from 'react'
+import { useRef} from 'react'
 import Btn from '../../styled/Btn'
 import './index.scss'
 
+
 const Calendar = () => {
-    // const [currentView, setCurrentView] = useState('');
-    // const [pluginView, setPluginView] = useState()
-    // let viewHandler = (view:string) => {
-    //     switch (view){
-    //         case 'month':
-    //             setCurrentView('dayGridMonth');
-    //             console.log(currentView);
-    //             break;
-    //         case 'week':
-    //             setCurrentView('timeGridWeek');
-    //             console.log(currentView);
-    //             break;
-    //         case 'day':
-    //             setCurrentView('timeGridDay');
-    //             console.log(currentView);
-    //             break;
-    //         default:
-    //             setCurrentView('dayGridMonth');
-    //     }
-    // }
-    let calendar = useRef();
-    calendar.current.getApi()
+    let calendarRef = useRef<FullCalendar | null>(null);
+    let changeView = (view:string) =>{
+        if(calendarRef.current){
+            let calendarApi = calendarRef.current.getApi();
+            calendarApi.changeView('timeGridDay');
+        }
+    }
     return (
         <div className='calendar__wrapper'>
             <div className="calendar">
                 <div className="btn__block">
-                    <Btn onClick={()=>viewHandler('month')}>Month</Btn>
-                    <Btn onClick={()=>viewHandler('week')}>Week</Btn>
-                    <Btn onClick={()=>viewHandler('day')}>Day</Btn>
+                    <Btn onClick={() => changeView('month')}>Month</Btn>
+                    <Btn onClick={() => changeView('month')}>Week</Btn>
+                    <Btn onClick={() => changeView('month')}>Day</Btn>
                 </div>
-                <FullCalendar plugins={[ timeGridPlugin ]}  />
+                <FullCalendar ref={calendarRef} plugins={[ timeGridPlugin, dayGridPlugin ]} initialView="dayGridMonth" />
             </div>
 
         </div>
